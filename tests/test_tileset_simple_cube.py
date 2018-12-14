cb4539671920f35fdf20355f6addd070e05670dd
EricBoix <eric.boix@insa-lyon.fr>
2018-12-14 10:51:05 +0100

    Cuboid definition now conforms to glTF's y-up

--- tests/test_tileset_simple_cube.py
+++ tests/test_tileset_simple_cube.py
@@ -7,6 +7,7 @@ class TestTileBuilder(unittest.TestCase, object):
 
     @staticmethod
     def build_cuboid_as_binary_triangles_array(org_x, org_y, org_z, dx, dy, dz):
+        # The following Cuboid definition assumes a z-up coordinate system.
         # Vertices order is such that normals are pointing outwards of the cube.
         # Each face of the cube is made of two triangles.
         return [
@@ -75,19 +76,29 @@ class TestTileBuilder(unittest.TestCase, object):
             'normal':   ts.getNormalArray(),
             'bbox':     ts.getBboxAsFloat()
         }]
-        transform = np.identity(4).flatten('F')
+        # GlTF uses a y-up coordinate system, and we thus need to realize
+        # a z-up to y-up coordinate transform for the cuboids to respect
+        # glTF convention (refer to
+        # https://github.com/AnalyticalGraphicsInc/3d-tiles/tree/master/specification#gltf-transforms
+        # for more details on this matter).
+        transform = np.array([ 1,  0,  0, 0,
+                               0,  0, -1, 0,
+                               0,  1,  0, 0,
+                               0,  0,  0, 1])
         glTF = GlTF.from_binary_arrays(arrays, transform)
         tile_content = B3dm.from_glTF(glTF)
         tile.set_content(tile_content)
 
         # Define the tileset that will hold the (single) tile
         tile_set = TileSet()
-        # Hardwiring :-( this tileset to the city of Lyon in EPSG:3946 refer to
+        # Hardwiring :-( a translation of this TileSet to the coordinates of
+        # the city of Lyon in EPSG:3946 refer to
         # https://epsg.io/map#srs=3946&x=1841276.446781&y=5172616.229943&z=14&layer=streets
         tile_set.set_transform([1, 0, 0, 0,
                                 0, 1, 0, 0,
                                 0, 0, 1, 0,
                                 1841276.4464434995, 5172616.229383407, 0, 1])
+
         tile_set.add_tile(tile)
         tile_set.add_asset_extras("Py3dTiles TestTileBuilder example.")
         tile_set.write_to_directory('junk')
