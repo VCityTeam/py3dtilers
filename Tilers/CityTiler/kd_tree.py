bd11ad2f2bd5744b6f334df1e6c71c10745e6fb0
yannlaurickabe <yannlaurick.abe@gmail.com>
2019-07-22 14:42:49 +0200

    Apply classes name modification in kd_tree function

--- Tilers/CityTiler/kd_tree.py
+++ Tilers/CityTiler/kd_tree.py
@@ -1,7 +1,7 @@
-from cityobject import CityObjects
+from citym_cityobject import CityMCityObjects
 
 
-def kd_tree(cityobjects, maxNumCityObjects, depth=0):
+def kd_tree(cityobjects, maxNumCityMCityObjects, depth=0):
     # The module argument of 2 (in the next line) hard-wires the fact that
     # this kd_tree is in fact a 2D_tree.
     axis = depth % 2
@@ -9,17 +9,17 @@ def kd_tree(cityobjects, maxNumCityObjects, depth=0):
     # Within the sorting criteria point[1] refers to the centroid of the
     # bounding boxes of the city objects. And thus, depending on the value of
     # axis, we alternatively sort on the X or Y coordinate of those centroids:
-    sCityObjects = CityObjects(
+    sCityMCityObjects = CityMCityObjects(
                     sorted(cityobjects,
                     key=lambda building: building.getCentroid()[axis]))
-    median = len(sCityObjects) // 2
-    lCityObjects = sCityObjects[:median]
-    rCityObjects = sCityObjects[median:]
-    pre_tiles = CityObjects()
-    if len(lCityObjects) > maxNumCityObjects:
-        pre_tiles.extend(kd_tree(lCityObjects, maxNumCityObjects, depth + 1))
-        pre_tiles.extend(kd_tree(rCityObjects, maxNumCityObjects, depth + 1))
+    median = len(sCityMCityObjects) // 2
+    lCityMCityObjects = sCityMCityObjects[:median]
+    rCityMCityObjects = sCityMCityObjects[median:]
+    pre_tiles = CityMCityObjects()
+    if len(lCityMCityObjects) > maxNumCityMCityObjects:
+        pre_tiles.extend(kd_tree(lCityMCityObjects, maxNumCityMCityObjects, depth + 1))
+        pre_tiles.extend(kd_tree(rCityMCityObjects, maxNumCityMCityObjects, depth + 1))
     else:
-        pre_tiles.append(lCityObjects)
-        pre_tiles.append(rCityObjects)
+        pre_tiles.append(lCityMCityObjects)
+        pre_tiles.append(rCityMCityObjects)
     return pre_tiles
