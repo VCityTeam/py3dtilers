7ecce37f9dc11447ec242f20033510d0f216bfe0
yannlaurickabe <yannlaurick.abe@gmail.com>
2019-07-22 14:40:51 +0200

    Change classes names to follow the CityGML model

---
+++ Tilers/CityTiler/citym_building.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from citym_cityobject import CityMCityObject
+
+
+class CityMBuilding(CityMCityObject):
+    def __init__(self):
+        super().__init__()
---
+++ Tilers/CityTiler/citym_relief.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from cityobject import CityMCityObject
+
+
+class CityMRelief(CityMCityObject):
+    """
+    Implementation of the Digital Terrain Model (DTM) objects from the CityGML model.
+    """
+    def __init__(self):
+        super().__init__()
