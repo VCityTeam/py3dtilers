39b23180b47ed3b995aeb5b8fa45aff56b88d353
EricBoix <eric.boix@insa-lyon.fr>
2020-01-21 11:49:02 +0100

    Ditch attempt to fix psycopg2.OperationalError.

--- Tilers/CityTiler/database_accesses.py
+++ Tilers/CityTiler/database_accesses.py
@@ -39,7 +39,27 @@ def open_data_base(db_config_file_path):
                 db_config['PG_NAME']),
         # fetch method will return named tuples instead of regular tuples
         cursor_factory=psycopg2.extras.NamedTupleCursor,
+        # Refer to note standing after this fuction call on why those
+        # keepalives parameters are here required.
+        keepalives=1,
+        keepalives_idle=30,
+        keepalives_interval=10,
+        keepalives_count=5
     )
+    # Why using the keepalives flags in the above psycopg2 constructor ?
+    # In the context of having to tile cities, such a connection can be 
+    # used for bulk querries (think of retrieving the geometries of all the 
+    # buildings of a large city). And it seems that dealing with bulk
+    # querries in a dockerized context tends to (randomly) produce the
+    # following error:
+    #    psycopg2.OperationalError: server closed the connection unexpectedly
+    #    This probably means the server terminated abnormally
+    #    before or while processing the request.
+    # For documented reports on this refer to
+    #   - https://stackoverflow.com/questions/55457069/how-to-fix-operationalerror-psycopg2-operationalerror-server-closed-the-conn
+    #   - https://stackoverflow.com/questions/53188306/python-sqlalchemy-connection-pattern-disconnected-from-the-remote-server
+    # The keepalives solution is provided by this StackOverflow:
+    # https://stackoverflow.com/questions/56289874/postgres-closes-connection-during-query-after-a-few-hundred-seconds-when-using-p
 
     try:
         # Open a cursor to perform database operations
