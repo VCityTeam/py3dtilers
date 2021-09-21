5d2bad86c288b74801300d5d41a23a01cd2994a0
Clement COLIN <clement.colin@liris.cnrs.fr>
2021-09-21 11:45:33 +0200

    added simple test for .obj

---
+++ tests/test_objTiler.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+from py3dtiles import BoundingVolumeBox
+
+from py3dtilers.ObjTiler.ObjTiler import from_obj_directory
+
+
+
+class Test_Tile(unittest.TestCase):
+
+    def test_basic_case(self):
+        path = 'tests/obj_tiler_data'
+
+        if not os.path.exists('tests/obj_tiler_data/generated_tilesets'):
+            os.makedirs('tests/obj_tiler_data/generated_tilesets')
+
+        tileset = from_obj_directory(path)
+        if(tileset is not None):
+            tileset.get_root_tile().set_bounding_volume(BoundingVolumeBox())
+            folder_name = "basic_case"
+            print("tileset in tests/obj_tiler_data/generated_tilesets/" + folder_name)
+            tileset.write_to_directory("tests/obj_tiler_data/generated_tilesets/" + folder_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
