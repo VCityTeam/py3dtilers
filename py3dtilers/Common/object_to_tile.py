393bcc27e10d73f5934566d5fca0de60fa1bee20
Clement COLIN <clement.colin@liris.cnrs.fr>
2021-11-17 11:18:24 +0100

    fix bounding box computation

--- py3dtilers/Common/object_to_tile.py
+++ py3dtilers/Common/object_to_tile.py
@@ -69,7 +69,7 @@ class ObjectToTile(object):
         # Set centroid from Bbox center
         self.centroid = np.array([(bbox[0][0] + bbox[1][0]) / 2.0,
                                   (bbox[0][1] + bbox[1][1]) / 2.0,
-                                  (bbox[0][2] + bbox[0][2]) / 2.0])
+                                  (bbox[0][2] + bbox[1][2]) / 2.0])
 
     def get_texture(self):
         return self.texture
