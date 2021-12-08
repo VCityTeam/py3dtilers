Fix uvs for OBJ and fix scaling

py3dtilers/Common/object_to_tile.py
@@ -144,9 +144,9 @@ class ObjectsToTile(object):
             centroid[0] += objectToTile.get_centroid()[0]
             centroid[1] += objectToTile.get_centroid()[1]
             centroid[2] += objectToTile.get_centroid()[2]
-        return [centroid[0] / self.get_size(),
+        return np.array([centroid[0] / self.get_size(),
                 centroid[1] / self.get_size(),
-                centroid[2] / self.get_size()]
+                centroid[2] / self.get_size()])
 
     def translate_objects(self, offset):
         """
@@ -187,10 +187,11 @@ class ObjectsToTile(object):
         :param transformer: the transformer used to change the crs
         :return:
         """
+        centroid = self.get_centroid()
         for object_to_tile in self.get_objects():
             new_geom = []
             for triangle in object_to_tile.get_geom_as_triangles():
-                scaled_triangle = [vertex * scale_factor for vertex in triangle]
+                scaled_triangle = [((vertex - centroid) * scale_factor) + centroid for vertex in triangle]
                 new_geom.append(scaled_triangle)
             object_to_tile.set_triangles(new_geom)
             object_to_tile.set_box()

py3dtilers/ObjTiler/obj.py
@@ -41,18 +41,27 @@ class Obj(ObjectToTile):
 
         vertices = mesh.materials[0].vertices
         length = len(vertices)
-        # If the mesh doesn't have a texture
+        # Contains only vertex positions
         if mesh.materials[0].vertex_format == 'V3F':
             for i in range(0, length, 9):
                 triangle = [np.array(vertices[n:n + 3], dtype=np.float32) for n in range(i, i + 9, 3)]
                 triangles.append(triangle)
-        # If the mesh has a texture
+        # Contains texture and vertex positions
+        elif mesh.materials[0].vertex_format == 'T2F_V3F':
+            for i in range(0, length, 15):
+                triangle = [np.array(vertices[n:n + 3], dtype=np.float32) for n in range(i + 2, i + 17, 5)]
+                triangles.append(triangle)
+                uv = [np.array([vertices[n], 1 - vertices[n + 1]], dtype=np.float32) for n in range(i, i + 15, 5)]
+                uvs.append(uv)
+        # Contains texture/vertex positions and normals        
         elif mesh.materials[0].vertex_format == 'T2F_N3F_V3F':
             for i in range(0, length, 24):
                 triangle = [np.array(vertices[n:n + 3], dtype=np.float32) for n in range(i + 5, i + 29, 8)]
                 triangles.append(triangle)
-                uv = [np.array(vertices[n:n + 2], dtype=np.float32) for n in range(i, i + 24, 8)]
+                uv = [np.array([vertices[n], 1 - vertices[n + 1]], dtype=np.float32) for n in range(i, i + 24, 8)]
                 uvs.append(uv)
+        else:
+            return False
 
         self.geom.triangles.append(triangles)
         if len(uvs) > 0: