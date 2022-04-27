9844ef857014a3f1a74e23f0cbb8856ba353fc40
EricBoix <eric.boix@insa-lyon.fr>
2022-04-27 15:57:17 +0200

    CityTiler is now properly exposed.
    Exposing Tiler was a mistake since it is already exposed in Common.

--- py3dtilers/CityTiler/__init__.py
+++ py3dtilers/CityTiler/__init__.py
@@ -4,11 +4,11 @@ from .citym_relief import CityMReliefs
 from .citym_waterbody import CityMWaterBodies
 from .citym_bridge import CityMBridges
 from .CityTiler import main
-from .CityTiler import Tiler
+from .CityTiler import CityTiler
 from .CityTemporalTiler import main as main_temporal
 from .database_accesses import open_data_base
 
 __all__ = ['CityMCityObjects', 'CityMBuildings', 'CityMReliefs',
            'CityMWaterBodies', 'CityMBridges',
-           'main', 'Tiler', 'main_temporal',
+           'main', 'CityTiler', 'main_temporal',
            'open_data_base']
