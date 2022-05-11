bf21bc3eadf2de5475c41c05ebe84e337d551ebe
Lorenzo Marnat <lmarnat14@gmail.com>
2022-05-11 10:36:30 +0200

    polish: delete old comments

--- py3dtilers/Texture/atlas_node.py
+++ py3dtilers/Texture/atlas_node.py
@@ -171,6 +171,4 @@ class Node(object):
             for y in range(0, 3):
                 new_u = (uvs[i][y][0] * ratioWidth) + offsetWidth
                 new_v = (uvs[i][y][1] * ratioHeight) + offsetHeight
-                # warning : in order to be written correctly, the GLTF writter
-                # expects data to be in float32
                 uvs[i][y] = np.array([new_u, new_v])
