5d48d8ec9ddf4c59f7aefb56830ee414890fc6d0
Charly <charly@perspective-s.org>
2023-01-26 10:53:36 +0100

    devMode test

--- py3dtilers/ObjTiler/ObjTiler.py
+++ py3dtilers/ObjTiler/ObjTiler.py
@@ -37,6 +37,7 @@ def main():
     and a classes.txt that contains the name of all tilesets
     :return: no return value
     """
+    print("devMode")
     obj_tiler = ObjTiler()
     obj_tiler.parse_command_line()
 
